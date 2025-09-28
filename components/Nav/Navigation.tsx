5c5f97f10a4849286efbd2daace10e6d24e7cf00
Enoch <enochagbloe16@gmail.com>
2025-09-29 00:53:22 +0100

    chore: Logout function implemented

--- components/Nav/Navigation.tsx
+++ components/Nav/Navigation.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -11,32 +11,76 @@ import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import { Button } from "../ui/button";
+import { auth, signOut } from "@/auth";
+import { LogOut } from "lucide-react";
+import { ROUTES } from "@/constant/route";
 
-const MobileNavigation = () => {
+const MobileNavigation = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Menu />
-      </SheetTrigger>
-      <SheetContent side="left">
-        <SheetHeader>
-          <SheetTitle>
-            <Link href={"/"} className="flex items-center gap-2 mt-4">
-            <Image
-              src={"/images/logo/main-logo.png"}
-              height={30}
-              width={30}
-              alt="logo"
-            />
-            <p>C3 Ignite ERP</p>
-            </Link>
-            <section className="mt-10 h-full flex-col flex gap-2 pt-3 scrollbar-hide">
-                <NavLinks isMobile/>
-            </section>
-          </SheetTitle>
-        </SheetHeader>
-      </SheetContent>
-    </Sheet>
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Menu />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>
+              <Link href={"/"} className="flex items-center gap-2 mt-4">
+                <Image
+                  src={"/images/logo/main-logo.png"}
+                  height={30}
+                  width={30}
+                  alt="logo"
+                />
+                <p>C3 Ignite ERP</p>
+              </Link>
+              <section className="mt-10 h-full flex-col flex gap-3 pt-4 scrollbar-hide">
+                <NavLinks isMobile />
+              </section>
+              <SheetClose>
+                <div className="flex">
+                  {userId ? (
+                    <SheetClose asChild>
+                      <form
+                        action={async () => {
+                          "use server";
+                          await signOut();
+                        }}
+                      >
+                        <Button className="flex py-2 px-12 w-full">
+                          <LogOut /> <span>Logout</span>
+                        </Button>
+                      </form>
+                    </SheetClose>
+                  ) : (
+                    <>
+                      <SheetClose asChild>
+                        <Link href={ROUTES.SIGN_IN}>
+                          <Button className="small-medium btn-secondary mt-12 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                            <p className="primary-text-gradient"> Log in </p>
+                          </Button>
+                        </Link>
+                      </SheetClose>
+
+                      <SheetClose asChild>
+                        <Link href={ROUTES.SIGN_UP}>
+                          <Button className="small-medium btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                            <p className="primary-text-gradient"> Sign up </p>
+                          </Button>
+                        </Link>
+                      </SheetClose>
+                    </>
+                  )}
+                </div>
+              </SheetClose>
+            </SheetTitle>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </>
   );
 };
 
