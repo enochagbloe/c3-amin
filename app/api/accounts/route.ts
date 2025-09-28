80c041556d11ba99c1662bf22511975df7b6060c
Enoch <enochagbloe16@gmail.com>
2025-09-28 16:59:30 +0100

    chore: Api for accounts

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,52 @@
+// we have to get all the list of account
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handler/error";
+import { ForbiddenError } from "@/lib/http.error";
+import dbConnect from "@/lib/mongoose";
+import { SignUpSchema } from "@/lib/validations";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const accounts = await Account.find();
+    return NextResponse.json(
+      {
+        success: true,
+        data: accounts,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// lets have an Api that creates a new account
+
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+    const body = req.json();
+    const validateData = SignUpSchema.safeParse(body);
+
+    //check for an existing Account
+    const existingAccount = await Account.findOne({
+      provider: validateData.data?.email,
+    });
+
+    if (existingAccount)
+      throw new ForbiddenError("Account with the same email already exist");
+
+    // create new account
+    const newAccount = await Account.create(validateData);
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    handleError(error, "api") as APIErrorResponse;
+  }
+}
